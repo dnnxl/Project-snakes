f1d13b3f9c98375b17f3f88d58511854e863ee13
beatletux <erickqf7@gmail.com>
2020-03-16 21:02:36 -0600

    imágenes

---
+++ snake/lib/Begin.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Begin extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        padding: EdgeInsets.only(top: 30, bottom: 10, right: 418, left: 60),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('images/actividad_main.jpg'),
+              //fit: BoxFit.cover,
+              alignment: Alignment.center),
+        ),
+        child: Column(
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: RaisedButton(
+                        color: Colors.green,
+                        shape: new RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0)),
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/encr");
+                        },
+                        child: SizedBox(
+                          width: 70,
+                          height: 30,
+                          child: Center(
+                            child:
+                            Text("En Costa Rica", textAlign: TextAlign.center),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: RaisedButton(
+                        color: Colors.green,
+                        shape: new RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0)),
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/aprendamos");
+                        },
+                        child: SizedBox(
+                          width: 70,
+                          height: 30,
+                          child: Center(
+                            child:
+                            Text("Aprendamos", textAlign: TextAlign.center),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: RaisedButton(
+                        color: Colors.green,
+                        shape: new RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0)),
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/contactos");
+                        },
+                        child: SizedBox(
+                          width: 70,
+                          height: 30,
+                          child: Center(
+                            child:
+                            Text("Contactos", textAlign: TextAlign.center),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: RaisedButton(
+                        color: Colors.green,
+                        shape: new RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0)),
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/acercade");
+                        },
+                        child: SizedBox(
+                          width: 70,
+                          height: 30,
+                          child: Center(
+                            child:
+                            Text("Acerca de", textAlign: TextAlign.center),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
